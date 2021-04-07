edc86a3baddeb94ab0c72fff87ff4f044bd3d124
Kyra McIntosh <takyramcintosh012@gmail.com>
2021-04-07 21:00:13 +0000

    Playing with python pip packages

---
+++ color/color3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import crayons
+
+def main():
+
+    """Runtime code."""
+
+    print(crayons.magenta('I\'m a Barbie girl!'))
+
+
+    print(crayons.blue('Blueeeee looks good on the sky'))
+
+    print(crayons.cyan('Hey'), crayons.yellow('there,'), crayons.magenta('Delilah'))
+
+main()
--- montypython/monty_python3.py
+++ montypython/monty_python3.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
-round = 0
-answer = " "
+""" fill this in """
 
-while round < 3 and answer  != "Stuck":
-    round += 1 
-    answer = input('Finish the song lyrics," If it\'s up then it\'s -----!": ')
-    if answer == 'Stuck':
-        print('Correct')
-    elif round == 3:
-        print("Sorry the answer was Stuck.")
-    else:
-        print("Sorry! Try again!")
+def main():
+    """ runtime code """
+    counter = 0
+    answer = " "
+
+    while counter < 3 and answer  != "Stuck":
+        counter += 1
+        answer = input('Finish the song lyrics," If it\'s up then it\'s -----!": ')
+        if answer == 'Stuck':
+            print('Correct')
+        elif counter == 3:
+            print("Sorry the answer was Stuck.")
+        else:
+            print("Sorry! Try again!")
+
+main()
